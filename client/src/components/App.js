9001453c29d5bdaec7632736a58bc894ce04235a
Mohan <mohansisodiya50@gmail.com>
2018-10-22 19:20:50 +0530

    fix for build error-3

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,7 @@ import * as actions from '../actions'
 import Header from './Header'
 import Landing from './Landing';
 
-const Dashboard = () => <h2>Dashboard</h2>
+const Dashboard = () => <h2>Dashboardddd</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 
 class App extends Component {
