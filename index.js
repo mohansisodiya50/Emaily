3ade982bd1ee8a00c352ed0defc41bd70f237628
Mohan <mohansisodiya50@gmail.com>
2018-10-22 20:33:36 +0530

    bug fixing

--- index.js
+++ index.js
@@ -9,12 +9,9 @@ require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
-// 
-
 const app = express();
 
-app.use(bodyParser.json())
-
+app.use(bodyParser.json());
 app.use(
 	cookieSession({
 		maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -28,12 +25,16 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'))
+	// Express will serve up production assets
+	// like our main.js file, or main.css file!
+	app.use(express.static('client/build'));
 
-	const path = require('path')
+	// Express will serve up the index.html file
+	// if it doesn't recognize the route
+	const path = require('path');
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
-	})
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
 }
 
 const PORT = process.env.PORT || 5000;
