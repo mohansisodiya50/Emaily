7d905631d47695fe13e98ac3b99c765c55103ef6
Mohan <mohansisodiya50@gmail.com>
2018-10-22 13:58:38 +0530

    adding billing and client side app

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import 'materialize-css/dist/css/materialize.min.css'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk'
+
+import App from './components/App'
+import reducers from './reducers'
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
+
+ReactDom.render(
+	<Provider store={store}><App /></Provider>,
+	document.querySelector('#root')
+)
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -9,6 +10,8 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(bodyParser.json())
 
 app.use(
 	cookieSession({
@@ -20,6 +23,16 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'))
+
+	const path = require('path')
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+	})
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
